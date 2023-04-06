409619e3775658324bbb58e89e55694bb94d2bc2
angelilton <epifanio.rds@gmail.com>
2023-04-06 15:11:36 -0400

    conf init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Onboarding from './src/Authentication/Onboarding';
 
-export default function App() {
+const AuthenticationStack = createStackNavigator()
+
+const AuthenticationScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthenticationStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <AuthenticationStack.Screen name='Onboarding' component={Onboarding} />
+    </AuthenticationStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AuthenticationScreen />
+    </NavigationContainer>
+  );
+}
---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+
+const Onboarding = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'cyan' }}>
+      <Text>hello Dev</Text>
+    </View>
+  );
+}
+
+export default Onboarding;
